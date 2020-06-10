9c308ce20a5d98ddf0b08c75f0b47236bf3ea2bb
jfigueroa <george_805@msn.com>
2020-06-10 18:53:09 -0500

    se implementa la misma lógica del dataflow en un script de node.

--- Sockets/pubsub.js
+++ Sockets/pubsub.js
@@ -1,7 +1,7 @@
 const { from } = require('rxjs');
 const { PubSub } = require('@google-cloud/pubsub');
 const { Observable } = require('rxjs');
-const { bufferTime, map, tap } = require('rxjs/operators');
+const { tap } = require('rxjs/operators');
 
 
 const pubSubClient = new PubSub();
@@ -21,7 +21,7 @@ const entrada = (io) => {
 
 const salida = (io) => {
 
-    const susbcripcion = pubSubClient.subscription('receptor');
+    const susbcripcion = pubSubClient.subscription('mapa1');
     const mensajes = new Observable(sub => {
         susbcripcion.on('message', (msg) => {
             sub.next(JSON.parse(msg.data.toString()));
@@ -29,33 +29,7 @@ const salida = (io) => {
         })
     })
 
-    mensajes.pipe(
-        //tap(info => console.log(info))
-        bufferTime(1000),
-        map(arr => Array.from(new Set(arr.map(c => c.nombre))).map(nombre => arr.find(e => e.nombre == nombre))),
-        map(arr => {
-          let coords = arr.reduce((recolector, mobil) => {
-            recolector.push({
-              geometry: {
-                coordinates: [mobil.longitud, mobil.latitud],
-                type: 'Point'
-              },
-              type: 'Feature',
-              properties: {
-                nombre: mobil.nombre,
-                aceleracion: mobil.aceleracion
-              }
-            });
-            return recolector;
-          }, [])
-      
-          return {
-            type: 'FeatureCollection',
-            features: coords
-          };
-        })
-        ,tap(info => console.log(JSON.stringify(info)))
-      ).subscribe(dat => io.sockets.emit('process', JSON.stringify(dat)))
+    mensajes.subscribe(dat => io.sockets.emit('process', JSON.stringify(dat)))
 }
 
 module.exports = { entrada, salida};
